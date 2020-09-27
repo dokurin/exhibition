51c2c0729f75c1d4229d839fc6f97e713ef5bd38
ryutah <ryuta0221@gmail.com>
2020-09-27 20:15:24 +0900

    chore: リポジトリで管理してるnpm開発ツールをインストールする用に変更 #55

--- build/develop/build.sh
+++ build/develop/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-cd $(dirname $0)
+cd $(dirname $0)/../../
 
-docker image build -t dokurin/exibition-devenv:1.0.0 .
+docker image build -f ./build/develop/Dockerfile -t dokurin/exibition-devenv:1.0.0 .
