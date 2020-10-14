7861caad25fdd6fed0f4a77909b5c4001c264d06
ryutah <ryuta0221@gmail.com>
2020-10-14 22:17:37 +0900

    chore: v-forを学習 #67

--- example/ryutah/hello-vue/index.js
+++ example/ryutah/hello-vue/index.js
@@ -4,7 +4,13 @@ const app = new Vue({
   el: "#app",
   data: {
     message: "Hello, Vue!",
+    todos: [
+      { text: "Learn JavaScript" },
+      { text: "Learn Vue" },
+      { text: "Build something awesome" },
+    ],
   },
 });
 
 app.message = "I have changed the data";
+app.todos.push({ text: "New Item" });
