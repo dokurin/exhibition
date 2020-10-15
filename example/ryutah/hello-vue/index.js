3d979bee530ef580e6c029d4b02af41228dcc291
ryutah <ryuta0221@gmail.com>
2020-10-15 21:58:39 +0900

    chore: vuexのhello, world

--- example/ryutah/hello-vue/index.js
+++ example/ryutah/hello-vue/index.js
@@ -1,6 +1,23 @@
 import Vue from "vue";
+import Vuex from "vuex";
 import TodoItem from "./todo_item";
 import Hello from "./Hello";
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    count: 0,
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    },
+  },
+});
+
+store.commit("increment");
+console.log(store.state.count);
 
 const app = new Vue({
   el: "#app",
