773485a764fc6f0ccdf4650702c7e389f8ba7bd9
ryutah <ryuta0221@gmail.com>
2020-10-16 23:53:57 +0900

    chore: vuexによる状態管理のサンプルを追加 #69

--- example/ryutah/hello-vue/index.js
+++ example/ryutah/hello-vue/index.js
@@ -1,22 +1,66 @@
 import Vue from "vue";
-import Vuex from "vuex";
+import Vuex, { mapState, mapGetters, mapMutations } from "vuex";
 import TodoItem from "./todo_item";
 import Hello from "./Hello";
 
 Vue.use(Vuex);
 
-const store = new Vuex.Store({
-  state: {
-    count: 0,
+const INCREMENT = "INVREMENT";
+const DECREMENT = "DECREMENT";
+
+const moduleA = {
+  namespaced: true,
+  state: () => ({
+    value: "this is default value",
+  }),
+  getters: {
+    reverseValue(state) {
+      return state.value
+        .split("")
+        .reverse()
+        .join("");
+    },
   },
   mutations: {
-    increment(state) {
+    setValue: (state, val) => (state.value = val),
+  },
+  actions: {
+    valueChange({ commit }, val) {
+      commit("setValue", val);
+    },
+  },
+};
+
+const store = new Vuex.Store({
+  modules: {
+    moduleA,
+  },
+  state: {
+    count: 0,
+    todos: [
+      { id: 1, text: "...", done: true },
+      { id: 2, text: "...", done: false },
+    ],
+  },
+  getters: {
+    doneTodos: (state) => state.todos.filter((todo) => todo.done),
+    doneTodoCount: (_, getters) => getters.doneTodos.length,
+    getTodoById: (state) => (id) => state.todos.find((todo) => todo.id === id),
+  },
+  mutations: {
+    [INCREMENT](state) {
       state.count++;
+    },
+    [DECREMENT](state) {
+      state.count--;
+    },
+  },
+  actions: {
+    incrementAsync({ commit }) {
+      setTimeout(() => commit(INCREMENT), 1000);
     },
   },
 });
-
-store.commit("increment");
 
 const app = new Vue({
   el: "#app",
@@ -30,6 +74,14 @@ const app = new Vue({
       { id: 1, text: "Cheese" },
       { id: 2, text: "Whatever else humans are supposed to eat" },
     ],
+    modA: {
+      value: "",
+    },
+  },
+  created() {
+    this.$data.modA = {
+      value: "default",
+    };
   },
   methods: {
     reverseMessage() {
@@ -38,14 +90,26 @@ const app = new Vue({
         .reverse()
         .join("");
     },
+    increment() {
+      this.$store.dispatch("incrementAsync");
+    },
+    ...mapMutations({
+      decrement: DECREMENT,
+    }),
+    valueChange() {
+      this.$store.dispatch("moduleA/valueChange", this.modA.value);
+    },
   },
   computed: {
     sayHello() {
       return `${this.greeting} ${this.name}!!`;
     },
-    count() {
-      return this.$store.state.count;
-    },
+    ...mapState(["count"]),
+    ...mapGetters(["doneTodos", "doneTodoCount", "getTodoById"]),
+    ...mapState({
+      moduleAValue: (state) => state.moduleA.value,
+    }),
+    ...mapGetters("moduleA", ["reverseValue"]),
   },
   components: {
     TodoItem,
